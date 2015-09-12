7ae23af2f57f8b340bada245c7ad79d4f769fc05
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 11:15:20 +0200

    Update routers

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 /* Team-Sport. */
 
-var express = require('express');
-var http = require('http');
+const express = require('express');
+const http = require('http');
 
-var app = express();
+const database = require('./database');
+const config = require('./config');
 
-app.get('/', (request, response) => {
-  response.sendFile('www/index.html');
-});
+const app = express();
+
+app.get('/', (request, response) => response.sendFile('public/index.html'));
+
+app.get('/login', (request, response) => response.sendFile('public/login.html'));
+
+app.get('/register', (request, response) => response.sendFile('public/register.html'));
+
+app.use('/api', require('./api'));
 
 http.createServer(app).listen(8080);

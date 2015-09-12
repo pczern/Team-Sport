c68891719c3d6778e243efa5b38ee1d738cc5921
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 14:34:32 +0200

    Add new /event/:id/enter router

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const database = require('./database');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const Promise = require('bluebird');
 
 const router = express.Router();
 
@@ -14,6 +15,20 @@ const userCookie = 'currentUser';
 
 //Body Parser Setup
 const urlEncoded = bodyParser.urlencoded({ extended: false });
+
+router.post('/login', urlEncoded, (request, response) => {
+  database.login(request.body.username, request.body.password)
+  .then((user) => {
+    response.cookie(userCookie, user).jsonp({success: true});
+  });
+  .catch((error) => response.jsonp({success: false, error: error}));
+});
+
+router.post('/register', urlEncoded, (request, response) => {
+  database.register(request.body.username, request.body.email, request.body.password)
+  .then(() => response.jsonp({success: true}));
+  .catch((error) => response.jsonp({success: false, error: error}));
+});
 
 router.use(cookieParser());
 
@@ -29,15 +44,13 @@ router.post('/add/location', urlEncoded, (request, response) => {
   .catch((error) => response.jsonp({success: false, error: error}));
 });
 
-router.post('/login', urlEncoded, (request, response) => {
-  database.login(request.body.username, request.body.password)
-  .then(() => response.jsonp({success: true}));
-  .catch((error) => response.jsonp({success: false, error: error}));
-});
+router.post('/location/:id/enter', (request, response) => {
+  Promise.try(function(){
+    if(!request.cookies[userCookie])
+      throw 'You aren\'t logged in.';
 
-router.post('/register', urlEncoded, (request, response) => {
-  database.register(request.body.username, request.body.email, request.body.password)
-  .then(() => response.jsonp({success: true}));
+    return database.enterEvent(request.params.id, request.cookies[userCookie])
+  }).then(() => response.jsonp({success: true}))
   .catch((error) => response.jsonp({success: false, error: error}));
 });
 

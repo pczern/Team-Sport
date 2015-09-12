6827a37838e09f71b05359bac69737778214b4f2
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 14:35:39 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2,18 +2,20 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
 var Promise = require("bluebird");
 var config = require('./config');
-mongoose.connect(config.database);
+var hat = require('hat');
+
 var User = mongoose.model("User", new Schema(config.schemas.user));
-var Location = mongoose.model('Location', new Schema(config.schemas.location));
+var Event = mongoose.model('Event', new Schema(config.schemas.event));
+
+mongoose.connect(config.database);
 
 //Login
-
 exports.login = function(username, password) {
-
 	return User.findOne({
-		name = req.body.name
+		name: username
 	}).exec().then(function(user){
 		if (!user) {
 			throw "Benutzer existiert nicht!"
@@ -23,31 +25,51 @@ exports.login = function(username, password) {
 
 		return Promise.resolve(user);
 	});
-
-
 };
 
 //Registrieren
-
 exports.register = function(username, password, email) {
-	User.findOne({
+	return User.findOne({
 		name = req.body.name
-	}).exec()
-	.then(function(user){
+	}).exec().then(function(user){
 		if (user) {
 			throw "Benutzer existiert bereits!"
 		}
+
 		return new User({
 					name: username,
 					password: password,
 					email: email
-		}).save().exec()
-
-	})
+		}).save().exec();
+	});
 };
 
-exports.findLocations = function(){
-  return Location.find().exec();
+exports.findEvent = function(){
+  return Event.find().exec();
 }
 
-exports.addLocation
+exports.addEvent = function(object, user) { //object is the
+  var x = parseFloat(object.x);
+  var y = parseFloat(object.y);
+
+  var event = new Event({
+    coordinates: [y, x],
+    name: object.name, // ???
+    type: object.type,
+    description: object.description,
+    start: new Date(object.start),
+    end: new Date(object.end),
+    people: [user.id]
+  });
+
+  return event.save().exec();
+}
+
+//id = event id
+exports.enterEvent = function(id, user) { // adds your ID to the event people
+  return Event.find({id}).exec()
+  .then(function(event) {
+    event.people.push(user.id);
+    return event.save().exec();
+  });
+}

44eefe54a4f9eefc4aecae4567928a0f2aad81a5
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 18:14:59 +0200

    Fix merge conflict

--- public/assets/mainCtrl.js
+++ public/assets/mainCtrl.js
@@ -2,9 +2,8 @@
 angular.module('mainCtrl.runningApp',[])
     .controller('mainCtrl',['$scope', '$mdDialog',function($scope, $mdDialog){
         $scope.message  = "I'm awesome!!!";
-<<<<<<< HEAD
         $scope.map = { center: { latitude: 51, longitude: 7 }, zoom: 10 };
-        
+
         $scope.showSearch = function(ev) {
         $mdDialog.show({
             controller: dialogCtrl,
@@ -18,7 +17,7 @@ angular.module('mainCtrl.runningApp',[])
         }, function() {
             $scope.status = 'You cancelled the dialog.';
         }
-            console.log(searchText);     
+            console.log(searchText);
         );
       };
     }]);
@@ -34,8 +33,3 @@ function dialogCtrl($scope, $mdDialog) {
     $mdDialog.hide(answer);
   };
 };
-=======
-        
-    }]);
-
->>>>>>> 034c1277c57e106ee44c399b092479c1e51754c1

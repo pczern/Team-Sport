f7bcce4a1a765038f48f5d31a8a5d80cb84c1133
Qun <yoda13@live.de>
2015-09-12 17:57:45 +0200

    Attackööööööö

--- public/assets/mainCtrl.js
+++ public/assets/mainCtrl.js
@@ -1,5 +1,34 @@
 angular.module('mainCtrl.runningApp',[])
-    .controller('mainCtrl',['$scope',function($scope){
+    .controller('mainCtrl',['$scope', '$mdDialog',function($scope, $mdDialog){
         $scope.message  = "I'm awesome!!!";
         $scope.map = { center: { latitude: 51, longitude: 7 }, zoom: 10 };
+        
+        $scope.showSearch = function(ev) {
+        $mdDialog.show({
+            controller: dialogCtrl,
+            templateUrl: 'views/pFindEvent.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+        .then(function(searchText) {
+            $scope.status = 'You said the information was "' + searchText + '".';
+        }, function() {
+            $scope.status = 'You cancelled the dialog.';
+        }
+            console.log(searchText);     
+        );
+      };
     }]);
+
+function dialogCtrl($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+};

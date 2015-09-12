249028e5669680e967888090373fd58d85a071b7
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 12:58:13 +0200

    Adds database stuff

--- database.js
+++ database.js
@@ -3,3 +3,9 @@
 var mongoose = require('mongoose');
 
 var config = require('./config');
+
+var Location = mongoose.model('Location', new Schema(config.schemas.location));
+
+exports.findLocations = function(){
+  return Location.find().exec();
+}

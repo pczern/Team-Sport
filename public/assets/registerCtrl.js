87af09d86bfa296cfafbc5d24c36965bc44d8827
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 19:56:33 +0200

    Changes

--- public/assets/registerCtrl.js
+++ public/assets/registerCtrl.js
@@ -1,5 +1,5 @@
 angular.module('registerCtrl.runningApp', [])
-  .controller('registerCtrl', ['$scope', function($scope) {
+  .controller('registerCtrl', ['$scope', "$http", "$location", function($scope, $http, $location) {
     $scope.greeting = "Woohoo2!!!";
     $scope.register = function() {
       var user = {
@@ -7,14 +7,51 @@ angular.module('registerCtrl.runningApp', [])
         'email': $scope.email,
         'password': $scope.password
       };
-      $http.post("/api/register", user)
-        .then(function(response) {
+
+      $http({
+        method: 'POST',
+        url: '/api/register',
+        data: param(user),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function(response) {
+          console.log(response.data);
           if (response.data.success === true)
             $location.path("/main")
           else
-            alert(response.data.error);
+            alert(JSON.stringify(response.data.error));
         }, function(response) {
-          alert(response.data.error);
+          console.log(response);
+          alert(JSON.stringify(response.data.error));
         });
     }
   }]);
+
+  function param(obj) {
+  var query = '',
+    name, value, fullSubName, subName, subValue, innerObj, i;
+
+  for (name in obj) {
+    value = obj[name];
+
+    if (value instanceof Array) {
+      for (i = 0; i < value.length; ++i) {
+        subValue = value[i];
+        fullSubName = name + '[' + i + ']';
+        innerObj = {};
+        innerObj[fullSubName] = subValue;
+        query += param(innerObj) + '&';
+      }
+    } else if (value instanceof Object) {
+      for (subName in value) {
+        subValue = value[subName];
+        fullSubName = name + '[' + subName + ']';
+        innerObj = {};
+        innerObj[fullSubName] = subValue;
+        query += param(innerObj) + '&';
+      }
+    } else if (value !== undefined && value !== null)
+      query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+  }
+
+  return query.length ? query.substr(0, query.length - 1) : query;
+};

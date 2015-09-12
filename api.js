c572ee6d1b0ec6c2b758ee887e008a8276c167f2
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 12:05:54 +0200

    Add location API router

--- api.js
+++ api.js
@@ -1,13 +1,32 @@
 /* api.js - All API routes */
 
+const database = require('./database');
+
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-router.get('/users/current/', (request, response) => {
-  database.getCurrent()
-  .then(function(){
+//Cookie Setup
+const sessionCookie = 'currentSession';
+const userCookie = 'currentUser';
 
-  });
+//Body Parser Setup
+const urlEncoded = bodyParser.urlencoded({ extended: false });
+
+router.use(cookieParser());
+
+router.get('/find/locations', (request, response) => {
+  database.findLocations()
+  .then((locations) => response.jsonp(locations));
+  .catch((error) => response.jsonp({success: false, error: error}));
 });
+
+router.post('/add/location', urlEncoded, (request, response) => {
+  database.addLocation(request.body)
+  .then(() => response.jsonp({success: true}));
+  .catch((error) => response.jsonp({success: false, error: error}));
+});
+
 module.exports = router;

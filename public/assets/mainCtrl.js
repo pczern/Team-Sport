47bf1659f4bebbf786606d533c46d86bb49a479e
speedDeveloper <info@speedDeveloper.me>
2015-09-12 17:25:53 +0200

    Update map

--- public/assets/mainCtrl.js
+++ public/assets/mainCtrl.js
@@ -1,5 +1,9 @@
-angular.module('mainCtrl.runningApp',[])
-    .controller('mainCtrl',['$scope',function($scope){
-        $scope.message  = "I'm awesome!!!";
-        $scope.map = { center: { latitude: 45, longitude: 6 }, zoom: 8 };
-    }]);
+angular.module('mainCtrl.runningApp', [])
+    .controller('mainCtrl', ['$scope', function ($scope) {
+                $scope.message = "I'm awesome!!!";
+
+    }
+                             
+                             
+
+]);

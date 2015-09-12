090a8643db6b2ec56cc83f51080c9158facc0208
Gabriel <gabriel.tomitsuka@montreus.com>
2015-09-12 18:57:29 +0200

    Multiple changes

--- public/assets/registerCtrl.js
+++ public/assets/registerCtrl.js
@@ -1,22 +1,20 @@
-angular.module('registerCtrl.runningApp',[])
-    .controller('registerCtrl',['$scope',function($scope){
-        $scope.greeting = "Woohoo2!!!";
-        $scope.register = function(){
-            var user    = {
-                'name'      : $scope.username,
-                'email'     : $scope.email,
-                'password'  : $scope.password
-            };
-            $http.post("/api/register", user)
-            .then(function(response) {
-              var data = JSON.parse(response.data);
-
-              if(data.success === true)
-                $location.path("/main")
-              else
-                alert(data.error);
-            }, function(response) {
-                alert(JSON.parse(response).error);
-            });
-        }
-    }]);
+angular.module('registerCtrl.runningApp', [])
+  .controller('registerCtrl', ['$scope', function($scope) {
+    $scope.greeting = "Woohoo2!!!";
+    $scope.register = function() {
+      var user = {
+        'name': $scope.username,
+        'email': $scope.email,
+        'password': $scope.password
+      };
+      $http.post("/api/register", user)
+        .then(function(response) {
+          if (response.data.success === true)
+            $location.path("/main")
+          else
+            alert(data.error);
+        }, function(response) {
+          alert(response.data.error);
+        });
+    }
+  }]);
